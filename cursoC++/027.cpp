e17ca767e70685fd775d8d412c6b1716cd20465e
Gabriel Vitor yKold <108306401+yKold@users.noreply.github.com>
2024-01-17 13:35:13 -0300

    g

---
+++ cursoC++/027.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+    enum armas{
+    fuzil = 100, 
+    revolver = 8, 
+    rifle = 12, 
+    escopeta = 1
+    };
+    armas armaSel = revolver;
+    
+
+    cout << armaSel;
+
+
+    return 0;
+}
